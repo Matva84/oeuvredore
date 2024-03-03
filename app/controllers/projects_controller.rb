b5aaca52fe5423a9304f248ac926f21eb3db580c
francois lorfevre <francoislorfevre@hotmail.com>
2024-03-03 23:51:47 +0100

    route projects et index et show

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,36 @@
 class ProjectsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
+  def index
+    @user = current_user
+    @users = User.all
+    @projects = Project.all
+
+  end
+
+  def new
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def projects_params
+    params.require(:project).permit(:title, :descritpion, :address, :initial_start_at, :initial_end_at, :progress, :customer_budget, :total_expenses)
+  end
+
 end
