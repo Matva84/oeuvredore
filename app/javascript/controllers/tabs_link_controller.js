cb1c8b689ecbc3e10b9129bc6b9f5a3d817c704f
Mathieu VALLEAU <mathieuvalleau@hotmail.fr>
2024-03-05 07:24:13 +0100

    Onglets fonctionnesl

--- app/javascript/controllers/tabs_link_controller.js
+++ app/javascript/controllers/tabs_link_controller.js
@@ -3,28 +3,60 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
 static targets = ["contentPlanning", "contentBudget", "contentDocuments", "contentPhotos", "contentMessagerie"]
 
-connect() {
-  console.log("Hello from the tabs controller!")
-}
+  connect() {
+    console.log("Hello from the tabs controller!")
+  }
 
-revealPlanning() {
-  console.log(this.contentTarget)
-  this.contentTarget.element.classList.remove("d-none")
-}
-revealBudget() {
-  console.log(this.contentTarget)
-  this.contentTarget.element.classList.remove("d-none")
-}
-revealDocuments() {
-  console.log(this.contentTarget)
-  this.contentTarget.element.classList.remove("d-none")
-}
-revealPhotos() {
-  console.log(this.contentTarget)
-  this.contentTarget.element.classList.remove("d-none")
-}
-revealMessagerie() {
-  console.log(this.contentTarget)
-  this.contentTarget.element.classList.remove("d-none")
-}
+  hideAll() {
+    //this.contentPlanningTarget.classList.add("d-none")
+    //this.contentBudgetTarget.classList.add("d-none")
+    //this.contentDocumentsTarget.classList.add("d-none")
+    //this.contentPhotosTarget.classList.add("d-none")
+    //this.contentMessagerieTarget.classList.add("d-none")
+    targets.forEach(target => {
+      target.classList.add("d-none")
+    })
+  }
+
+  revealPlanning() {
+    this.contentPlanningTarget.classList.add("d-none")
+    this.contentBudgetTarget.classList.add("d-none")
+    this.contentDocumentsTarget.classList.add("d-none")
+    this.contentPhotosTarget.classList.add("d-none")
+    this.contentMessagerieTarget.classList.add("d-none")
+    this.contentPlanningTarget.classList.remove("d-none")
+  }
+  revealBudget() {
+    this.contentPlanningTarget.classList.add("d-none")
+    this.contentBudgetTarget.classList.add("d-none")
+    this.contentDocumentsTarget.classList.add("d-none")
+    this.contentPhotosTarget.classList.add("d-none")
+    this.contentMessagerieTarget.classList.add("d-none")
+    this.contentBudgetTarget.classList.remove("d-none")
+  }
+  revealDocuments() {
+    this.contentPlanningTarget.classList.add("d-none")
+    this.contentBudgetTarget.classList.add("d-none")
+    this.contentDocumentsTarget.classList.add("d-none")
+    this.contentPhotosTarget.classList.add("d-none")
+    this.contentMessagerieTarget.classList.add("d-none")
+    this.contentDocumentsTarget.classList.remove("d-none")
+  }
+  revealPhotos() {
+    this.contentPlanningTarget.classList.add("d-none")
+    this.contentBudgetTarget.classList.add("d-none")
+    this.contentDocumentsTarget.classList.add("d-none")
+    this.contentPhotosTarget.classList.add("d-none")
+    this.contentMessagerieTarget.classList.add("d-none")
+    this.contentPhotosTarget.classList.remove("d-none")
+  }
+  revealMessagerie() {
+    this.contentPlanningTarget.classList.add("d-none")
+    this.contentBudgetTarget.classList.add("d-none")
+    this.contentDocumentsTarget.classList.add("d-none")
+    this.contentPhotosTarget.classList.add("d-none")
+    this.contentMessagerieTarget.classList.add("d-none")
+    this.contentMessagerieTarget.classList.remove("d-none")
+  }
+
 }
