ec008941b45374b41d5e13094ad26f09b563d786
Mathieu VALLEAU <mathieuvalleau@hotmail.fr>
2024-03-08 08:11:47 +0100

    Chart.js reglé"

--- app/javascript/controllers/line_chart_controller.js
+++ app/javascript/controllers/line_chart_controller.js
@@ -1,24 +1,23 @@
 import { Controller } from "@hotwired/stimulus";
-import { Chart } from "chart.js";
+//import { Chart } from "chart.js";
 
 export default class extends Controller {
 
   connect() {
     console.log("Line chart controller connected")
 
-    const linechartChart = new Chart(this.element, {
-      type: 'bar',
-      data: {
-        labels: ['Men', 'Women'],
-        datasets: [{
-          label: 'Gender ratio',
-          data: [504, 496],
-          backgroundColor: ['rgb(255, 99, 132)', 'rgb(54, 162, 235)'],
-          hoverOffset: 4
-        }]
-      }
-    });
-}
+    //const linechartChart = new Chart(this.element, {
+    //  type: 'bar',
+    //  data: {
+    //    labels: ['Men', 'Women'],
+    //    datasets: [{
+    //      label: 'Gender ratio',
+    //      data: [504, 496],
+    //      backgroundColor: ['rgb(255, 99, 132)', 'rgb(54, 162, 235)'],
+    //      hoverOffset: 4
+    //    }]
+    //  }
+    };
 
   disconnect() {
     console.log("Line chart controller disconnected");
