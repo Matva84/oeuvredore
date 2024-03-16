2dca6a84875f60cd82c2f6d9fb3f42150135f913
Mathieu VALLEAU <mathieuvalleau@hotmail.fr>
2024-03-16 08:43:40 +0100

    Loading docs and photo

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,23 +1,44 @@
 class DocumentsController < ApplicationController
   def new
+    @project = Project.find(params[:project_id])
     @document = Document.new
   end
 
+  def index
+    @user = current_user
+    @users = User.all
+    @projects = Project.all
+    @docs_all = Document.all
+    @documents = []
+    @documents_all.each do |doc|
+      if document.user_id == @user.id
+        @documents << doc
+      end
+    end
+  end
+
   def create
-    @project = Project.find(params[:id])
+    puts "VOICI LE DOCUMENT CREE 1"
     tempfile = params[:file].tempfile
+    name = params[:file].original_filename
     upload = Cloudinary::Uploader.upload(tempfile.path, folder: 'pdf_uploads')
     @upload_url = upload['secure_url']
-    @document = Document.new(project_id: @project, name: "Test", type_of_document: "Photo", url: upload['secure_url'])
-    puts "VOICI LE DOCUMENT CREE"
-    puts @project
+    if ((name.include? ".jpg") || (name.include? ".jpeg") || (name.include? ".png"))
+      doctype = "Photo"
+    else
+      doctype = "Document"
+    end
+    filename = name.split('.')[0]
+    @document = Document.new(name: filename, type_of_document: doctype, url: upload['secure_url'])
+    @project = Project.find(params[:project_id])
+    @document.project_id = @project.id
     puts @document.project_id
+
     if @document.save
       puts ">>>>>>Document saved with succes"
     else
       puts ">>>>>>Document not saved"
     end
-    # render :new
   end
 
   def show
@@ -27,11 +48,7 @@ class DocumentsController < ApplicationController
 
   private
 
-  def project_params
-    params.require(:project).permit(:id)
-  end
-
   def document_params
-    params.require(:document).permit(:photo)
+    params.require(:document).permit(:name, :type_of_document, :url)
   end
 end
