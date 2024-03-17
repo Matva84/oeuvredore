70268a89aacaf86fbb37790099c213d80d31705d
Benoit Queyron <queyronbenoit@gmail.com>
2024-03-17 15:45:42 +0100

    done

--- app/javascript/controllers/line_chart_controller.js
+++ app/javascript/controllers/line_chart_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus";
 import { Chart } from "chart.js";
 
 export default class extends Controller {
-  static targets = ["tasksName", "tasksPrice"]
+  static targets = ["tasksName", "tasksPrice", "tasksProgress"]
 
   connect() {
 
@@ -14,44 +14,42 @@ export default class extends Controller {
     var prices = this.tasksPriceTarget.dataset.value
     var prices_json = JSON.parse(prices)
 
+    var progress = this.tasksProgressTarget.dataset.value
+    var progress_json = JSON.parse(progress)
+
+    var progression_prices_data = [];
+    for (let i = 0; i < prices_json.length; i++) {
+      progression_prices_data.push((prices_json[i] * progress_json[i]/100));
+    }
+
     new Chart(
       this.element, {
       type: 'bar',
       data: {
         labels: names_json,
         datasets: [{
-          label: "",
+          label: "Prévisionnel",
           data: prices_json,
           backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(255, 159, 64, 0.2)',
-            'rgba(255, 205, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(201, 203, 207, 0.2)'
+            'rgba(255, 159, 64, 0.2)'
+          ]
+        }, {
+          label: "Réalisé",
+          data: progression_prices_data,
+          backgroundColor: [
+            'rgba(255, 159, 64, 1)'
           ],
-          borderColor: [
-            'rgb(255, 99, 132)',
-            'rgb(255, 159, 64)',
-            'rgb(255, 205, 86)',
-            'rgb(75, 192, 192)',
-            'rgb(54, 162, 235)',
-            'rgb(153, 102, 255)',
-            'rgb(201, 203, 207)'
-          ],
-          borderWidth: 1,
-          hoverOffset: 4
-        }]
+        }
+      ]
       },
       options: {
         scales: {
-            yAxes: [{
-                scaleLabel: {
-                    display: true,
-                    labelString: 'Prix (€)'
-                }
-            }]
+          x: {
+            stacked: true,
+          },
+          y: {
+            stacked: false,
+          }
         }
       }
     });
