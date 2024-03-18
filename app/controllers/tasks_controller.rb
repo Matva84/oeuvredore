59eaea383b3fe9d319f6d5a274500ac2e76b474b
Benoit Queyron <queyronbenoit@gmail.com>
2024-03-18 14:09:42 +0100

    install flatpicker

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,29 @@
 class TasksController < ApplicationController
   def new
+    @project = Project.find(params[:project_id])
     @task_new = Task.new
   end
 
   def create
+    raise
+    @project = Project.find(params[:project_id])
     @task_new = Task.new
+    # construire la fonction date
   end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  private
+
+  def params_task
+    params.require(:task).permit(:name, :description, :category, :start_at, :end_at, :progress, :budget, :photo)
+  end
+
+
 end
